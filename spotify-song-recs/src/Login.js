0377da05a0cad711e5b472935367cbd487b2f77a
Elijah <elijah10kim@gmail.com>
2024-06-08 15:15:27 -0400

    update homepage layout and design

--- spotify-song-recs/src/Login.js
+++ spotify-song-recs/src/Login.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import CallToAction from './CallToAction';
 
 function Login() {
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Song Recs for Spotify</h1>
-                <a className="btn-spotify" href="/auth/login" >
-                    Login with Spotify
-                </a>
             </header>
+            <CallToAction />
+            <a className="btn-spotify" href="/auth/login" >
+                    Login with Spotify
+            </a>
         </div>
     );
 }
