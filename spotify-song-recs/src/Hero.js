316fd1ebd45797a3f804f5d5833c78f5ce84d474
Elijah <elijah10kim@gmail.com>
2024-06-08 17:08:29 -0400

    add background image

--- spotify-song-recs/src/Hero.js
+++ spotify-song-recs/src/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className='hero'>
             <p id = "message">Discover the perfect songs for every moment by customizing your search. Tailor your music experience and find exactly what you want to listen to with ease.</p>
-            <p>Filter songs by <span className= "parameters" style={{color: "#ff0000"}}>danceabilitiy</span>, <span className= "parameters" style={{color: "white"}}>energy</span>, <span className= "parameters" style={{color: "blue"}}>tempo</span> and more!</p>
+            <p>Filter songs by <span className= "parameters" style={{color: "rgb(226, 50, 50)"}}>danceabilitiy</span>, <span className= "parameters" style={{color: "rgb(187, 71, 187)"}}>energy</span>, <span className= "parameters" style={{color: "rgb(73, 68, 212)"}}>tempo</span> and more!</p>
         </div>
     );
 }
